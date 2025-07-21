ceaef8f28164dea6f2ad78694a174068ee04521c
White Torn <FaustusTorn@protonmail.com>
2025-07-21 13:11:57 +0400

    Mark 0 - add google gemini node | load api key from .env file.

--- Mark_1.py
+++ Mark_1.py
@@ -1,9 +1,16 @@
+import os
+from langchain.chat_models import init_chat_model
+
 from typing import Annotated
 
 from typing_extensions import TypedDict
 
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+
+os.environ["GOOGLE_API_KEY"] = "..."
+
+llm = init_chat_model("google_genai:gemini-2.0-flash")
 
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
