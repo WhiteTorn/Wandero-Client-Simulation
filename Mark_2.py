83d9b3b2bc6f4fe84206d2bc0e9f917b5409d979
White Torn <FaustusTorn@protonmail.com>
2025-07-21 14:45:31 +0400

    Mark 0 - Customizing states with more complex agen logic

---
+++ Mark_2.py
@@ -0,0 +1,86 @@
+from langchain_core.messages import ToolMessage
+from langchain_core.tools import InjectedToolCallId, tool
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+import os
+
+from typing import Annotated
+
+from langchain_tavily import TavilySearch
+from langchain_core.messages import ToolMessage
+from langchain_core.tools import InjectedToolCallId, tool
+from typing_extensions import TypedDict
+
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.types import Command, interrupt
+
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+search_key = os.getenv("TAVILY_API_KEY")
+
+os.environ["GOOGLE_API_KEY"] = api_key
+os.environ["TAVILY_API_KEY"] = search_key
+
+llm = init_chat_model("google_genai:gemini-2.0-flash")
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+    name: str
+    birthday: str
+
+@tool
+def human_assistance(
+    name: str, birthday: str, tool_call_id: Annotated[str, InjectedToolCallId]
+) -> str:
+    """Request assistance from a human."""
+    human_response = interrupt(
+        {
+            "question": "Is this correct?",
+            "name": name,
+            "birthday": birthday,
+        },
+    )
+    if human_response.get("correct", "").lower().startswith("y"):
+        verified_name = name
+        verified_birthday = birthday
+        response = "Correct"
+    else:
+        verified_name = human_response.get("name", name)
+        verified_birthday = human_response.get("birthday", birthday)
+        response = f"Made a correction: {human_response}"
+
+    state_update = {
+        "name": verified_name,
+        "birthday": verified_birthday,
+        "messages": [ToolMessage(response, tool_call_id=tool_call_id)],
+    }
+    return Command(update=state_update)
+
+tool = TavilySearch(max_results=2)
+tools = [tool, human_assistance]
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    message = llm_with_tools.invoke(state["messages"])
+    assert(len(message.tool_calls) <= 1)
+    return {"messages": [message]}
+
+graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=tools)
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
--- README.md
+++ README.md
@@ -9,4 +9,7 @@
 
 1. Found `llms-full.txt` for the LangGraph which size was 160.000 Tokens.
 2. Found also `llms.txt` which was links to the separate documentation files, planning to ask claude which modules we will require and load in model context only that docs, because of context.
-3. 
+
+3. Builded basic chatbot to learn logic [[Mark_1.py]]
+
+4. Builded Custom State Management [[Mark_2.py]]
