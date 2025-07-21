dc41d19de0cb867feba94455506764056fe376e5
White Torn <FaustusTorn@protonmail.com>
2025-07-21 13:25:06 +0400

    Mark 0 - First tests and successfull first chatbot

--- Mark_1.py
+++ Mark_1.py
@@ -1,4 +1,5 @@
 import os
+from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
 
 from typing import Annotated
@@ -8,7 +9,10 @@ from typing_extensions import TypedDict
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
 
-os.environ["GOOGLE_API_KEY"] = "..."
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+os.environ["GOOGLE_API_KEY"] = api_key
 
 llm = init_chat_model("google_genai:gemini-2.0-flash")
 
@@ -18,5 +22,34 @@ class State(TypedDict):
     messages: Annotated[list, add_messages]
 
 graph_builder = StateGraph(State)
+
+def chatbot(state:State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
 
 # StateGraph - state machine 
